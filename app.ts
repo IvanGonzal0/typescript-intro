15bbfb4275bdcf8ffc40021b9505f5462f43af75
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-12-15 15:11:59 -0300

    Types

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
 console.log('Esto es codigo en typescript');
+
+function add(number1: number, number2: number) {
+    console.log(typeof number1);
+    console.log(typeof number2);
+    
+    
+    return number1 + number2;
+}
+
+var number1 = 5;
+var number2 = 2.8;
+
+const result = add(number1,number2)
+console.log(result);
