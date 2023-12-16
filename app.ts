3a0bad3c7fe3faaa1d758e19e84f5e1dff02a598
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-12-16 19:03:45 -0300

    number, string & boolean

--- app.ts
+++ app.ts
@@ -1,15 +1,25 @@
 console.log('Esto es codigo en typescript');
 
-function add(number1: number, number2: number) {
+function add(number1: number, number2: number, showResult: boolean, phrase: string) {
     console.log(typeof number1);
     console.log(typeof number2);
+    console.log(typeof showResult);
+    console.log(typeof resultPhrase);
     
+    const result = number1 + number2;
     
+    if (showResult) {
+        console.log(phrase + result);
+    }else{
+        return number1 + number2;
+    }
     return number1 + number2;
 }
 
 var number1 = 5;
 var number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1,number2)
+const result = add(number1, number2, printResult, resultPhrase)
 console.log(result);
