fd3d4ac77ac3ba4573a7e5fd0b0a25c1f7b74b21
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-12-23 10:38:38 -0300

    Object Types

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-console.log('Esto es codigo en typescript');
-function add(number1, number2, showResult, phrase) {
-    console.log(typeof number1);
-    console.log(typeof number2);
-    console.log(typeof showResult);
-    console.log(typeof resultPhrase);
-    var result = number1 + number2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return number1 + number2;
-    }
-    return number1 + number2;
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-var result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+var person = {
+    name: 'Maximilian',
+    age: 30
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,25 +1,9 @@
-console.log('Esto es codigo en typescript');
-
-function add(number1: number, number2: number, showResult: boolean, phrase: string) {
-    console.log(typeof number1);
-    console.log(typeof number2);
-    console.log(typeof showResult);
-    console.log(typeof resultPhrase);
-    
-    const result = number1 + number2;
-    
-    if (showResult) {
-        console.log(phrase + result);
-    }else{
-        return number1 + number2;
-    }
-    return number1 + number2;
+const person: {
+    name: string,
+    age: number
+} = {
+    name: 'Maximilian',
+    age: 30
 }
 
-var number1 = 5;
-var number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-const result = add(number1, number2, printResult, resultPhrase)
-console.log(result);
+console.log(person);
