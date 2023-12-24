ed3001f0c508e8854b439454d46ce797e968d870
Ivan Gonzalo Tapia <108291957+IvanGonzal0@users.noreply.github.com>
2023-12-24 12:52:50 -0300

    Enums

--- app.js
+++ app.js
@@ -2,11 +2,31 @@
 //     name: string,
 //     age: number
 // } = {
+// const person: {
+//     name:string,
+//     age:number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+// const ADMIN = 'ADMIN';
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role["AUTHOR"] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 var favoriteActivities;
 favoriteActivities = ['Sports'];
@@ -14,4 +34,7 @@ console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toLocaleLowerCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
--- app.ts
+++ app.ts
@@ -2,16 +2,31 @@
 //     name: string,
 //     age: number
 // } = {
-const person: {
-    name:string,
-    age:number,
-    hobbies: string[],
-    role: [number, string]
-} = {
+// const person: {
+//     name:string,
+//     age:number,
+//     hobbies: string[],
+//     role: [number, string]
+// } = {
+//     name: 'Maximilian',
+//     age: 30,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']
+// }
+
+// const ADMIN = 'ADMIN';
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role{
+    ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR'
+}
+
+const person = {
     name: 'Maximilian',
     age: 30,
     hobbies: ['Sports', 'Cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 }
 
 let favoriteActivities: string[];
@@ -19,6 +34,10 @@ favoriteActivities = ['Sports'];
 
 console.log(person.name);
 
-for (const hobby of person.hobbies){
+for (const hobby of person.hobbies) {
     console.log(hobby.toLocaleLowerCase());
+}
+
+if (person.role === Role.ADMIN){
+    console.log('is admin');
 }
